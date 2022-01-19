0295e7e9a93f285c0208f1d59aa9b65969a02a32
gustavo fajardo <megatavo60@gmail.com>
2022-01-19 16:07:34 -0500

    config deploy

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import Template from "./templates/Template.js";
-import "./styles/main.css";
-import "./styles/vars.styl";
+import Template from "@templates/Template.js";
+import "@styles/main.css";
+import "@styles/vars.styl";
 
+console.log('hola');
 
 (async function App() {
   const main = null || document.getElementById("main");
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,8 +2,6 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const Dotenv = require("dotenv-webpack");
 
@@ -14,6 +12,10 @@ module.exports = {
     filename: "[name].[contenthash].js",
     assetModuleFilename: "assets/images/[hash][ext][query]",
   },
+
+  mode: "development",
+  watch: true,
+
   resolve: {
     extensions: [".js"],
     alias: {
@@ -70,8 +72,4 @@ module.exports = {
     new CleanWebpackPlugin(),
     new Dotenv(),
   ],
-  optimization: {
-    minimize: true,
-    minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
-  },
 };
